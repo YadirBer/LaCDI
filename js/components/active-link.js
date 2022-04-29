6e1bd9cd88e8c975afb6ad9f6cdad6f611e98b4d
Yadir Bernal <104169027+YadirBer@users.noreply.github.com>
2022-04-28 22:01:10 -0500

    Menu Hamburger Actualización del menú de hamburguesa en todas las secciones

---
+++ js/components/active-link.js
@@ -0,0 +1,8 @@
+/* export function activeLink(links) {
+  const d = document,
+    $links = d.querySelectorAll(links);
+  $path = window.location.href;
+
+  d.addEventListener();
+}
+ */
--- js/lacdi.js
+++ js/lacdi.js
@@ -1,2 +1,5 @@
 import { hamburgerMenu } from "./components/hamburguer.js";
+/* import { activeLink } from "./components/active-link.js"; */
+
+/* activeLink(".menu a"); */
 hamburgerMenu(".menu", ".hamburger");
